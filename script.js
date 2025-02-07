a2444844db283e90b49022836e609bc4de2a5b6d
codephoenix86 <nareshlohar066@gmail.com>
2025-02-07 16:12:50 +0530

    localstorage bugs fixed

--- script.js
+++ script.js
@@ -22,16 +22,18 @@ class TaskLog {
   }
   delete(id) {
     const i = this.#getTask(id);
-    this.arr.splice(i - 1, 1);
+    this.arr.splice(i, 1);
+    if (this.isEmpty())
+      this.counter = 1;
   }
   #getTask(id) {
     for (let i in this.arr) {
-      if (this.arr[i].id == id)
-        return i;
+      if (this.arr[i].id == id) return i;
     }
   }
-  insert(arr) {
-    this.arr = arr;
+  isEmpty() {
+    if (this.arr.length == 0) return true;
+    else return false;
   }
   tasks() {
     return this.arr;
@@ -40,34 +42,31 @@ class TaskLog {
 
 const elements = {
   tasklog: document.querySelector(".tasklog"),
-  add: document.querySelector('.add'),
-  input: document.querySelector('.task-input'),
+  add: document.querySelector(".add"),
+  input: document.querySelector(".task-input"),
   createInput: function (value) {
     const html = `<input type="text" value="${value}" class="task-name">`;
     const element = parse(html);
     return element;
   },
   createTask: function (task) {
-    const html = 
-    `<div class="task">
+    const html = `<div class="task">
       <input type="checkbox" class="task-checkbox"/>
       <p class="task-name" contenteditable="true">${task.name}</p>
       <input type="button" id=${task.id} class="btn delete" value="delete" />
-    </div>`
+    </div>`;
     const element = parse(html);
-    const del = element.querySelector('.delete');
-    del.addEventListener('click', function (event) {
-      console.log('delete button is clicked')
-      console.log(tasklog)
+    const del = element.querySelector(".delete");
+    del.addEventListener("click", function (event) {
       tasklog.delete(del.id);
       displayTasks(tasklog);
     });
     return element;
-  }
+  },
 };
 
 function parse(html) {
-  const element = document.createElement('div');
+  const element = document.createElement("div");
   element.innerHTML = html;
   return element.firstElementChild;
 }
@@ -80,16 +79,18 @@ function displayTasks(tasklog) {
   });
   storeTasks(tasklog);
 }
-function storeTasks(tasklog) 
-{
-  const jsonString = JSON.stringify(tasklog);
-  localStorage.setItem('tasklog', jsonString);
+function storeTasks(tasklog) {
+  if (tasklog.isEmpty())
+    localStorage.removeItem('tasklog');
+  else
+  {
+    const jsonString = JSON.stringify(tasklog);
+    localStorage.setItem("tasklog", jsonString);
+  }
 }
 
-function restoreTasks()
-{
-  const obj = JSON.parse(localStorage.getItem('tasklog'));
-  console.log(obj)
+function restoreTasks() {
+  const obj = JSON.parse(localStorage.getItem("tasklog"));
   return Object.assign(new TaskLog(), obj);
 }
 
@@ -99,28 +100,25 @@ function addTask(name) {
 }
 
 function isValidTask(name) {
-  if (name == '')
-    return false;
+  if (name == "") return false;
   return true;
 }
 
 const tasklog = restoreTasks();
 displayTasks(tasklog);
 
-elements.add.addEventListener('click', function (event) {
+elements.add.addEventListener("click", function (event) {
   const name = elements.input.value;
-  if (isValidTask(name))
-  {
+  if (isValidTask(name)) {
     addTask(name);
-    elements.input.value = '';
+    elements.input.value = "";
   }
 });
 
-elements.input.addEventListener('keydown', function (event) {
+elements.input.addEventListener("keydown", function (event) {
   const name = elements.input.value;
-  if (event.key == 'Enter' && isValidTask(name))
-  {
+  if (event.key == "Enter" && isValidTask(name)) {
     addTask(name);
-    elements.input.value = '';
+    elements.input.value = "";
   }
-})
+});
